df90fbf248cbfbcc62893d8ce52f6177a8d3741a
PatriciaCR <patriciacr219@gmail.com>
2020-09-12 23:51:45 -0700

    update flix

---
+++ Flix/Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Patricia Caceres on 9/10/20.
+//  Copyright © 2020 Patricia Caceres. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+}
